ce93dd455d8740d52e9bffcb88bf46aae8322574
tincanman <tiansky.yao95@gmail.com>
2015-09-19 23:19:33 -0400

    Added def for main REPL loop

--- A2/game.ml
+++ A2/game.ml
@@ -1,12 +1,12 @@
 #require "yojson"
 open Yojson
-
+(*Record for an exit*)
 type exit =
   {
     direction     : string;
     room          : string;
   }
-
+(*Record for a room*)
 type room =
   {
     id            : string;
@@ -16,30 +16,30 @@ type room =
     exits         : exit list;
     treasures     : string list;
   }
-
+(*Record for an item*)
 type item =
   {
     id            : string;
     description   : string;
     points        : int;
   }
-
+(*Helper function for parsing an "exit" json object*)
 let parse_exit e =
   let open Yojson.Basic.Util in
   {direction    = e |> (member "direction") |> to_string;
    room         = e |> (member "room") |> to_string;}
-
+(*Helper function for parsing an array of "exit" json object*)
 let rec parse_exits jsonExits =
   match jsonExits with
   | [] -> []
   | hd::tl -> parse_exit hd :: parse_exits tl
-
+(*Helper function for parsing an item json object*)
 let parse_item i =
   let open Yojson.Basic.Util in
   {id           = to_string (member "id" i);
    description  = to_string (member "description" i);
    points       = to_int (member "points" i);}
-
+(*Helper function for parsing a room json object*)
 let parse_room r =
   let open Yojson.Basic.Util in
   {id           = to_string (member "id" r);
@@ -48,17 +48,17 @@ let parse_room r =
    points       = to_int (member "points" r);
    exits        = parse_exits (flatten (filter_member "exits" [r]));
    treasures    = filter_string (filter_member "treasure" [r]);}
-
+(*Helper function for parsing an array of "item" json object*)
 let rec parse_items jsonItems =
   match jsonItems with
   | [] -> []
   | hd::tl -> parse_item hd :: parse_items tl
-
+(*Helper function for parsing an array "room" json object*)
 let rec parse_rooms jsonRooms =
   match jsonRooms with
   | [] -> []
   | hd::tl -> parse_room hd :: parse_rooms tl
-
+(*Function extracts an array of all "room" json objects*)
 let extract_rooms (t : Yojson.Basic.json) : room list =
   let open Yojson.Basic.Util in
   [t]
@@ -93,3 +93,9 @@ let items = extract_items t
 let rooms = extract_rooms t
 let startRoom = extract_start_room t
 let startItems = extract_start_items t
+
+let rec main_game_loop (visited_rooms : string list)
+  (item_locations : (string * string) list) (current_room : string)
+  (inventory : string list) (points : int) (turns : int)
+  (*Read the input*)
+  let input = read_line ()
