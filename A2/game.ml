342b5df4ff942838d91cbe68ccc1a165c02418a8
tincanman <tiansky.yao95@gmail.com>
2015-09-24 14:29:17 -0400

    Take, drop, score, and turn printing added; Added starting score

--- A2/game.ml
+++ A2/game.ml
@@ -88,25 +88,39 @@ let extract_start_items (t: Yojson.Basic.json) : string list =
   |> flatten
   |> filter_string
 
-let t = Yojson.Basic.from_file "minimal.json"
+let t = Yojson.Basic.from_file "gates.json"
 let items = extract_items t
 let rooms = extract_rooms t
 let start_room = extract_start_room t
 let start_items = extract_start_items t
 (*The max number of points that can be earned*)
 let max_points =
-  let get_treasure_pts treasure =
-    (List.hd (List.filter (fun i -> i.id = treasure) items)).points in
-  List.fold_left
-  (fun pts (room:room_rec) -> room.points +
-    (List.fold_left
-      (fun pts tre -> get_treasure_pts tre + pts) 0 room.treasures)
-     + pts) 0 rooms
+  (List.fold_left (fun pts (room:room_rec) -> room.points + pts) 0 rooms) +
+  (List.fold_left (fun pts (item:item_rec) -> item.points + pts) 0 items)
 (*The list of item locations at the start of the game*)
 let start_item_locations =
   List.flatten (List.fold_left
   (fun i_loc_list (room:room_rec) ->
     (List.map (fun item -> (item, room.id)) room.items)::i_loc_list) [] rooms)
+(*The starting points*)
+let start_points =
+  let calc_item_points item_id room_id =
+    let room = List.hd
+      (List.filter (fun (room:room_rec) ->
+      (String.lowercase room.id) = (String.lowercase room_id)) rooms) in
+    if List.filter (fun tre ->
+      (String.lowercase tre) = (String.lowercase item_id)) room.treasures <> []
+    then (List.hd (List.filter
+      (fun i ->
+        (String.lowercase i.id) = (String.lowercase item_id)) items)).points
+    else 0 in
+  (*The points of the starting room*)
+  ((List.hd (List.filter
+    (fun (room:room_rec) -> room.id = start_room) rooms)).points) +
+  (*Points of items already in their designated rooms*)
+  (List.fold_left (fun pts (item_loc:string*string) ->
+    (calc_item_points (fst item_loc) (snd item_loc)) + pts)
+    0 start_item_locations)
 
 let rec main_game_loop (visited_rooms : string list)
   (item_locations : (string * string) list) (current_room_id : string)
@@ -116,12 +130,9 @@ let rec main_game_loop (visited_rooms : string list)
   (*Tokenize the string for pattern matching*)
   let input_split = Str.bounded_split (Str.regexp "[ \t]+") input 2 in
   (*Get the room record for the current room*)
-  let current_room =
-    let rec get_cur_room (r:room_rec list) r_id=
-      match r with
-      | [] -> failwith "Room error exception"
-      | hd::tl -> if hd.id = r_id then hd else get_cur_room tl r_id in
-    get_cur_room rooms current_room_id in
+  let current_room = List.hd
+    (List.filter (fun (room:room_rec) ->
+      (String.lowercase room.id) = (String.lowercase current_room_id)) rooms) in
   (*Check whether the room with id = r_id has been visited*)
   let visited r_id =
     if List.filter (fun x -> x = r_id) visited_rooms <> []
@@ -129,7 +140,8 @@ let rec main_game_loop (visited_rooms : string list)
   (*Helper function to return the exit record for a movement direction
    *Returns a list. The list is empty for an invalid direction*)
   let get_exit move_input =
-    List.filter (fun x -> x.direction = move_input) current_room.exits in
+    List.filter (fun x ->
+      (String.lowercase x.direction) = move_input) current_room.exits in
   (*Helper function for printing out the item and item description
    *Takes in the id of the item, item_id*)
   let print_item item_id =
@@ -139,7 +151,9 @@ let rec main_game_loop (visited_rooms : string list)
    *Given a room_rec room, this function will print out the room description,
    *the list of items in the room, and the description of each item*)
   let print_room (room:room_rec) =
-    Printf.printf "%s\n" room.description;
+    Printf.printf "----------------------------------------------\n";
+    Printf.printf "You are now in %s\n\n" room.id;
+    Printf.printf "%s\n\n" room.description;
     (*Get the list of items that are in this room*)
     let items_in_room =
       List.map (fun i_loc -> fst i_loc)
@@ -156,7 +170,7 @@ let rec main_game_loop (visited_rooms : string list)
     if exit <> [] then
     (*Create a temporary variable for storing the next room room_rec*)
     let next_room = List.hd (List.filter
-        (fun (room:room_rec) -> room.id = (List.hd exit).room) rooms) in
+      (fun (room:room_rec) -> room.id = (List.hd exit).room) rooms) in
     (*Print out the room description for the next room*)
     print_room next_room;
     (*Call main loop again with the new room*)
@@ -177,19 +191,29 @@ let rec main_game_loop (visited_rooms : string list)
    *This function returns the item's points if it's in the designated room
    *and 0 otherwise*)
   let calc_item_points item_id =
-    if List.filter (fun tre -> tre = item_id) current_room.treasures <> []
-    then (List.hd (List.filter (fun i -> i.id = item_id) items)).points
+    if List.filter (fun tre ->
+      (String.lowercase tre) = item_id) current_room.treasures <> []
+    then (List.hd (List.filter
+      (fun i -> (String.lowercase i.id) = item_id) items)).points
     else 0 in
   (*Function for picking up an item
    *Function calls main_game_loop with new states to go to next turn*)
   let take_item item_id =
-    if List.filter (fun id -> id = item_id) current_room.items <> []
-    then main_game_loop
+    (*Temp variable for storing the actual name (case accurate) of the item*)
+    let item = List.filter (fun id ->
+      (String.lowercase id) = item_id) current_room.items in
+    if item <> []
+    then let item_points = calc_item_points item_id in
+      Printf.printf "You have taken %s and lost %d points\n\n"
+        (List.hd item) item_points;
+      main_game_loop
       visited_rooms
       (*Generate the new list of item locations with the taken item removed*)
-      (List.filter (fun it_loc -> fst it_loc <> item_id) item_locations)
-      current_room_id (item_id::inventory)
-      (points - (calc_item_points item_id))
+      (List.filter (fun it_loc ->
+        (String.lowercase (fst it_loc)) <> item_id) item_locations)
+      current_room_id
+      ((List.hd item)::inventory)
+      (points - item_points)
       (turns + 1)
     else Printf.printf "%s does not exist in this room.\n" item_id;
       main_game_loop
@@ -197,13 +221,19 @@ let rec main_game_loop (visited_rooms : string list)
   (*Function for dropping an item
    *Function calls main_game_loop with new states to go to next turn*)
   let drop_item item_id =
-    if List.filter (fun id -> id = item_id) inventory <> []
-    then main_game_loop
+    let item = List.filter (fun id ->
+      (String.lowercase id) = item_id) inventory in
+    if item <> []
+    then let item_points = calc_item_points item_id in
+      Printf.printf "You have taken %s and gained %d points\n\n"
+        (List.hd item) item_points;
+      main_game_loop
       visited_rooms
       (*Generate the new list of item locations with the dropped item added*)
-      ((item_id, current_room_id)::item_locations)
-      current_room_id (List.filter (fun id -> id <> item_id) inventory)
-      (points + (calc_item_points item_id))
+      (((List.hd item), current_room_id)::item_locations)
+      current_room_id
+      (List.filter (fun id -> (String.lowercase id) <> item_id) inventory)
+      (points + item_points)
       (turns + 1)
     else Printf.printf "%s is not in your inventory.\n" item_id;
       main_game_loop
@@ -218,11 +248,11 @@ let rec main_game_loop (visited_rooms : string list)
       main_game_loop visited_rooms item_locations
       current_room_id inventory points turns
     (*score command*)
-    | "score"::tl -> Printf.printf "Your score: %d\n" points;
+    | "score"::tl -> Printf.printf "Your score: %d\n\n" points;
       main_game_loop visited_rooms item_locations
       current_room_id inventory points turns
     (*turns command*)
-    | "turns"::tl -> Printf.printf "Number of turns taken: %d\n" turns;
+    | "turns"::tl -> Printf.printf "Number of turns taken: %d\n\n" turns;
       main_game_loop visited_rooms item_locations
       current_room_id inventory points turns
     (*inventory command*)
@@ -251,6 +281,5 @@ main_game_loop
   [start_room]
   start_item_locations
   start_room start_items
-  (List.hd (List.filter
-    (fun (room:room_rec) -> room.id = start_room) rooms)).points
+  start_points
   0
