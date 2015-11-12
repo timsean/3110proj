a8b08086bba40a5be74de8719c622404738fd87f
tincanman <tiansky.yao95@gmail.com>
2015-11-11 20:15:52 -0500

    Added support to popping multiple times on an empty queue

--- A5/aQueue.ml
+++ A5/aQueue.ml
@@ -6,16 +6,19 @@ let create () =
   ref []
 
 let is_empty q =
-  q = (create ())
+  (q = (create ())) ||
+  (List.fold_left (fun acc a -> acc && (a = Ivar.create ())) true (!q))
+
+let is_empty_reflst q = (q = (create ()))
 
 let push q x =
   let packed = Ivar.create () in
   Ivar.fill packed x;
-  if is_empty q then
+  if is_empty_reflst q then
     q := (!q) @ [packed]
   else
     if Ivar.is_empty (List.hd (!q)) then
-      (Ivar.fill (List.hd (!q)) x; q := [])
+      (Ivar.fill (List.hd (!q)) x; q := List.tl (!q))
     else
       q := (!q) @ [packed]
 
--- A5/tests.ml
+++ A5/tests.ml
@@ -1,3 +1,6 @@
+open Exercises
+open AQueue
+open Async.Std
 (******************************************************************************)
 (** Unit tests for Exercises **************************************************)
 (******************************************************************************)
@@ -17,13 +20,29 @@ TEST "any has unit tests" = failwith "TODO"
 (******************************************************************************)
 (** Unit tests for AQueue *****************************************************)
 (******************************************************************************)
-
 (** Note: you do not have to write tests for create *)
+TEST_MODULE "queue tests" = struct
+  let qtest = create ()
 
-TEST "push has unit tests" = failwith "TODO"
+  TEST "push0_isempty" = is_empty qtest
+  let _ = push qtest 1
+  TEST "push1_isnotempty" = not (is_empty qtest)
+  let _ = push qtest 2
+  TEST "push2_isnotempty" = not (is_empty qtest)
+  let _ = push qtest 3
+  TEST "push3_isnotempty" = not (is_empty qtest)
+  TEST "pop1" = (pop qtest = (return 1))
+  TEST "pop2" = (pop qtest = (return 2))
+  TEST "pop3" = (pop qtest = (return 3))
+  TEST "popped_all_isempty" = is_empty qtest
+  let deferredpop = pop qtest
+  TEST "before_async_pop_isempty" = is_empty qtest
+  let _ = push qtest 123
+  TEST "async_pop" = (deferredpop = (return 123))
+  TEST "after_async_pop_isempty" = is_empty qtest
 
-TEST "pop has unit tests" = failwith "TODO"
 
-TEST "is_empty has unit tests" = failwith "TODO"
+  TEST "pop has unit tests" = failwith "TODO"
 
-
+  TEST "is_empty has unit tests" = failwith "TODO"
+end
